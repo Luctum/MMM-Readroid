3aed20331a1f0ece3dda29f333cf55c431e23afb
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-09 09:14:28 +0100

    added a comment and post firebasehandler class

--- app/src/main/java/palla_boitard_mubanzo/readroid/models/Comment.kt
+++ app/src/main/java/palla_boitard_mubanzo/readroid/models/Comment.kt
@@ -1,6 +1,23 @@
 package palla_boitard_mubanzo.readroid.models
 
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
 data class Comment (val content : String?, val timestamp: String?)
+
+class FireBaseCommentHandler: FireBaseHandler<Comment> {
+    override fun getReference(ref: String): DatabaseReference {
+        return FirebaseDatabase.getInstance().reference.child(ref)
+    }
+
+    override fun getChild(ref: String, child: String): DatabaseReference {
+        return this.getReference(ref).child(child)
+    }
+
+    override fun create(ref: String, child: String?, obj: Comment) {
+        this.getChild(ref, child.toString()).setValue(obj)
+    }
+
+}
--- app/src/main/java/palla_boitard_mubanzo/readroid/models/Post.kt
+++ app/src/main/java/palla_boitard_mubanzo/readroid/models/Post.kt
@@ -1,6 +1,22 @@
 package palla_boitard_mubanzo.readroid.models
 
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
 data class Post (val user : User?, val comments: MutableList<Comment>?)
+
+class FireBasePostHandler: FireBaseHandler<Post> {
+    override fun getReference(ref: String): DatabaseReference {
+        return FirebaseDatabase.getInstance().reference.child(ref)
+    }
+
+    override fun getChild(ref: String, child: String): DatabaseReference {
+        return this.getReference(ref).child(child)
+    }
+
+    override fun create(ref: String, child: String?, obj: Post) {
+        this.getChild(ref, child.toString()).setValue(obj)
+    }
+}

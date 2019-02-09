d085198f2875a91296737eac1aacbf23b0c64c70
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-09 09:06:39 +0100

    FireBaseHandler strategy interface and one concrete Strategy for data class user.

--- app/src/main/java/palla_boitard_mubanzo/readroid/models/User.kt
+++ app/src/main/java/palla_boitard_mubanzo/readroid/models/User.kt
@@ -1,11 +1,30 @@
 package palla_boitard_mubanzo.readroid.models
 
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
 data class User(
     var username: String? = "",
     var email: String? = "",
-    var post : Post?,
-    val favorites : MutableList<Post>
+    var post: Post? = null,
+    val favorites: MutableList<Post>? = null
 )
+
+class FireBaseUserHandler : FireBaseHandler<User> {
+
+    override fun getReference(ref: String): DatabaseReference {
+        return FirebaseDatabase.getInstance().reference.child(ref)
+    }
+
+    override fun getChild(ref: String, child: String): DatabaseReference {
+        return getReference(ref).child(child)
+    }
+
+    override fun create(ref:String, child: String?, obj: User) {
+        val databaseReference = this.getChild(ref, child.toString())
+        databaseReference.setValue(obj)
+    }
+
+}

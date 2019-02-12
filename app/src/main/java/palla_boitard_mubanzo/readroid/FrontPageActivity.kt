82e29a3c2749377df5fcd744eead7972d5c051c6
Hugo PALLA <hugo.palla@gmail.com>
2019-02-12 22:06:58 +0100

    merge manifest

--- app/src/main/java/palla_boitard_mubanzo/readroid/FrontPageActivity.kt
+++ app/src/main/java/palla_boitard_mubanzo/readroid/FrontPageActivity.kt
@@ -3,12 +3,12 @@ package palla_boitard_mubanzo.readroid
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 
 import kotlinx.android.synthetic.main.front_page.*
 import palla_boitard_mubanzo.readroid.models.User
@@ -17,6 +17,7 @@ class FrontPageActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
+    private lateinit var profileReference: DatabaseReference
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +27,20 @@ class FrontPageActivity : AppCompatActivity() {
         this.auth = FirebaseAuth.getInstance()
         this.database = FirebaseDatabase.getInstance().reference
         val text = findViewById<TextView>(R.id.textLogin)
-        text.text = auth.currentUser!!.email
+
+        //Get the current user
+        profileReference = database.child("users").child(auth.currentUser!!.uid)
+        val profileListener = object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val user = dataSnapshot.getValue(User::class.java)
+                text.text = user!!.username
+            }
+        }
+        profileReference.addListenerForSingleValueEvent(profileListener)
     }
 
 }

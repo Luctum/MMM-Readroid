a9344e8cf5227b872bf542ead8d23444fadb10dd
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-12 16:53:12 +0100

    update on postActivity, added new properties

--- app/src/main/java/palla_boitard_mubanzo/readroid/PostActivity.kt
+++ app/src/main/java/palla_boitard_mubanzo/readroid/PostActivity.kt
@@ -17,8 +17,11 @@ class PostActivity : AppCompatActivity() {
         setContentView(R.layout.post_layout)
         this.fireBaseHandler = FireBasePostHandler()
         var content = findViewById<TextView>(R.id.postcontent)
-        val comment = findViewById<Button>(R.id.commentbtn)
+        val postTitle = findViewById<TextView>(R.id.postTitle)
+        val commentbtn = findViewById<Button>(R.id.commentbtn)
         val author = findViewById<TextView>(R.id.author)
+        content.text = "Lord Have mercy, I get that ich for sally. That me makes me caught up in the middle. Bobby Womack"
+        author.text = "Bobby Womack"
+        postTitle.text = "Caught Up In The Middle"
     }
-
 }

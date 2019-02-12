3c95437d9eaaf12182046259b9f565fa6b40299e
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-12 16:30:34 +0100

    update on comments adapter and added missing properties in data class files.

--- app/src/main/java/palla_boitard_mubanzo/readroid/adapter/CommentsViewAdapter.kt
+++ app/src/main/java/palla_boitard_mubanzo/readroid/adapter/CommentsViewAdapter.kt
@@ -1,15 +1,16 @@
 package palla_boitard_mubanzo.readroid.adapter
 
 import android.content.Context
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.comments_scrollview.view.*
+import android.widget.TextView
 import palla_boitard_mubanzo.readroid.R
 import palla_boitard_mubanzo.readroid.models.Comment
 
-class CommentsViewAdapter(val comments: MutableList<Comment>, val context : Context):RecyclerView.Adapter<CommentsViewAdapter.ViewHolder>() {
+class CommentsViewAdapter(private val comments: MutableList<Comment>, val context : Context):RecyclerView.Adapter<CommentsViewAdapter.ViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(context).inflate(R.layout.comments_scrollview, p0, false))
     }
@@ -19,10 +20,15 @@ class CommentsViewAdapter(val comments: MutableList<Comment>, val context : Cont
     }
 
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        p0.commentAuthor.text = comments[p1].author?.username
+        p0.commentItem.text = comments[p1].content
+        p0.commentTimestamp.text = comments[p1].timestamp
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val commentViewType = itemView.cardComment
+        val commentItem: TextView = itemView.findViewById(R.id.commentContent)
+        val commentAuthor :  TextView = itemView.findViewById(R.id.commentAuthor)
+        val commentTimestamp : TextView = itemView.findViewById(R.id.commentTimestamp)
+        val commentViewType : CardView = itemView.findViewById(R.id.cardComment)
     }
 }

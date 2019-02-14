2cebf611beebc613b853290400ba7aeb1c84e699
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-14 06:19:53 +0100

    comment dialog window and use of it in a a PostActivity.

--- app/src/main/java/palla_boitard_mubanzo/readroid/PostActivity.kt
+++ app/src/main/java/palla_boitard_mubanzo/readroid/PostActivity.kt
@@ -1,12 +1,14 @@
 package palla_boitard_mubanzo.readroid
 
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import palla_boitard_mubanzo.readroid.adapters.CommentsViewAdapter
 import palla_boitard_mubanzo.readroid.models.Comment
@@ -16,37 +18,50 @@ import palla_boitard_mubanzo.readroid.models.User
 class PostActivity : AppCompatActivity() {
 
     private lateinit var fireBaseHandler: FireBasePostHandler
-    private var tag: String = "PostActivity"
+    private var dialog : Dialog? = null
+    private val commentsContent: MutableList<Comment> = mutableListOf()
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        val postId = this.intent.getIntExtra("postId", -1)
-        if(postId == -1){
-
-        }else{
-            println(postId)
-        }
         super.onCreate(savedInstanceState)
-        Log.d(tag, "OnCreate started")
         setContentView(R.layout.post_layout)
         this.fireBaseHandler = FireBasePostHandler()
-        var content = findViewById<TextView>(R.id.postcontent)
+        val content = findViewById<TextView>(R.id.postcontent)
         val postTitle = findViewById<TextView>(R.id.postTitle)
         val commentbtn = findViewById<Button>(R.id.commentbtn)
         val author = findViewById<TextView>(R.id.author)
+        dialog = Dialog(this)
         content.text = "Lord Have mercy, I get that ich for sally. That me makes me caught up in the middle. Bobby Womack"
         author.text = "Bobby Womack"
         postTitle.text = "Caught Up In The Middle"
-        val commentsContent : MutableList<Comment> = mutableListOf()
-        commentsContent.add(Comment("yo", User("josh", "josh@gmail.com"), "12443213"))
-        initRecyclerView(commentsContent)
+        commentbtn.setOnClickListener {
+            showpopup(it)
+        }
     }
 
-    fun initRecyclerView(comments: MutableList<Comment>) {
+    private fun initRecyclerView(comments: MutableList<Comment>) {
         val recyclerView: RecyclerView = findViewById(R.id.comments_list)
         val commentsAdapter = CommentsViewAdapter(this, comments)
         recyclerView.adapter = commentsAdapter
         recyclerView.layoutManager = LinearLayoutManager(this)
     }
+
+    private fun showpopup(view:View) {
+        dialog?.setContentView(R.layout.comment_dialog_popup)
+        val editText : EditText? = dialog?.findViewById(R.id.inputComment)
+        val cancelBtn : Button? = dialog?.findViewById(R.id.cancelBtn)
+        val doCommentBtn: Button? = dialog?.findViewById(R.id.doCommentBtn)
+
+        cancelBtn?.setOnClickListener {
+            dialog?.dismiss()
+        }
+        doCommentBtn?.setOnClickListener {
+            commentsContent.add(Comment(editText!!.text.toString(), User("josh"), "12345"))
+            initRecyclerView(commentsContent)
+            dialog?.dismiss()
+        }
+
+        dialog?.show()
+    }
+
 }
